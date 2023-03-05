49426cbb7549f1d7cb02fa55c6093f4b1d5800c2
Raphael Yaadar <yaadar.5913211@gmail.com>
2023-03-05 22:07:31 +0000

    Project INit

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,10 @@
+import testimonials from "./testimonials";
+import abouts from "./abouts";
+import brands from "./brands";
+import contact from "./contact";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import works from "./works";
+import experiences from "./experiences";
+
+export const schemaTypes = [testimonials, abouts, brands, contact, skills, workExperience, works, experiences]
