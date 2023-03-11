35fdd7f7ed75bcbbfc5448995dee3b5033291bb3
Raphael Yaadar <yaadar.5913211@gmail.com>
2023-03-11 12:46:55 +0000

    Add fetching of About section data from sanity backend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,15 +2,29 @@ import React, {useState, useEffect} from 'react';
 import './About.scss';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
-  { title: 'Web Design', description: 'I am a good web developer', imgUrl: images.about02},
-  { title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03},
-  { title: 'Web Animations', description: 'I am a good web developer', imgUrl: images.about04},
-];
+// const abouts = [
+//   { title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
+//   { title: 'Web Design', description: 'I am a good web developer', imgUrl: images.about02},
+//   { title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03},
+//   { title: 'Web Animations', description: 'I am a good web developer', imgUrl: images.about04},
+// ];
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+   
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data));
+    
+  }, [])
+  
+
   return (
     <>
       <h2 className='head-text'>I Know that <span>Good Design</span> <br/> means <span>Good Business</span>
@@ -23,7 +37,7 @@ const About = () => {
                         transition={{ duration: 0.5, type:'tween' }}
                         className="app__profile-item"
                         key={about.title + index}>
-              <img src={about.imgUrl} alt={about.title} />
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className='bold-text' style={{ marginTop: 20 }}>
                 {about.title}
               </h2>
